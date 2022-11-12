d226efe4ce45a674b0e85497878b43d7c9a3d5cb
Pedro Amorim <pepas52@gmail.com>
2022-11-12 14:51:01 +0000

    added image slider

---
+++ slider.js
@@ -0,0 +1,18 @@
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("slides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].className = slides[i].className.replace(" slide-in", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    slides[slideIndex - 1].className += " slide-in";
+    setTimeout(showSlides, 5000); // Change image every 4 seconds
+}
